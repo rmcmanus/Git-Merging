c9baf3f1d1bc9594834f7f03f7555c15a8457dc5
Ryan McManus <rmcmanus@mines.edu>
2012-10-11 08:39:14 -0600

    Added main

--- Git_Merging.java
+++ Git_Merging.java
@@ -1,9 +1,16 @@
-public class test {
+public class Test {
    private static int n;
    private int z;
    
    public void update(int amt) {
       z = amt;
       n = amt;
+   }
+
+   public int main() {
+      Test test1 = new Test();
+      Test test2 = new Test();
+      test1.update(15);
+      test2.update(25);
    }
 }
